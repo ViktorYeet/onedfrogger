46929a370ce530c0b65e060891aeb89870697860
ViktorYeet <viktor@stefansbo.se>
2024-08-20 00:20:08 +0200

    its working :D

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,49 @@
-//use std::io;
-use std::fs;
+use std::io;
+//use std::fs;
 
 fn main() {
-    let data = file_to_string("inputdata.txt");
-    let mut linedata = data.lines();
+    //let data = file_to_string("inputdata.txt");
+    //let mut linedata = data.lines();
     
-    let settings = linedata.next().expect("something went wrong");
-    
-    let mut settings = settings.split(" ");
-    let board_size: usize = settings.next().unwrap().parse().unwrap();
-    let start_pos: i32 = settings.next().unwrap().parse().unwrap();
+    //let settings = linedata.next().expect("something went wrong");
+    let mut settings = String::new();
+    let stdin = io::stdin();
+    stdin.read_line(&mut settings).expect("there should be data here");
+
+    let mut settings = settings.trim().split(" ");
+    dbg!(&settings);
+    let board_size: i32 = settings.next().unwrap().parse().unwrap();
+    let start_pos: i32 = settings.next().unwrap().parse::<i32>().unwrap() -1;
     let win_value: i32 = settings.next().unwrap().parse().unwrap();
 
-    let board: Vec<&str> = linedata.next().expect("there should be data").split_whitespace().collect();
+    //let board: Vec<&str> = linedata.next().expect("there should be data").split_whitespace().collect();
+    let mut buffer = String::new();
+    stdin.read_line(&mut buffer).expect("there should be data here");
+    let board: Vec<&str> = buffer.split_whitespace().collect();
+
     let mut pos = start_pos.clone();
     let mut hops = 0;
+    let mut visited = Vec::new();
     loop {
         if board[pos as usize] == win_value.to_string() { 
             println!("magic\n{hops}");
             return; //you win,
         }
-        else if pos == start_pos && hops != 0 {
+        else if visited.contains(&pos) {
             println!("cycle\n{hops}");
             return; //going in circles
         }
-        else if board[pos as usize].parse::<usize>().unwrap() < 1 || board[pos as usize].parse::<usize>().unwrap() > board_size {
+        else if pos < 1 || pos > board_size {
             println!("falls\n{hops}");
             return // you lose
         }
-        pos = board[pos as usize].parse::<i32>().unwrap();
+        visited.push(pos);
+        pos += board[pos as usize].parse::<i32>().unwrap();
         hops += 1;
     }
-    
-
-    //println!("{}",board[start_pos]);
 }
 
-fn file_to_string(path: &str) -> String {
+/* fn file_to_string(path: &str) -> String {
     let content: String = fs::read_to_string(path).expect("should have read the file");
     content
-}
+} */
