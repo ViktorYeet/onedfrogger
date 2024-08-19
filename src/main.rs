22a2963709988cfa2918494ad1bac19e4fbc18de
ViktorYeet <viktor@stefansbo.se>
2024-08-19 22:40:49 +0200

    input works for settings

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
-use std::io;
+//use std::io;
 use std::fs;
 
 fn main() {
     let data = fileToString("inputdata.txt");
+    let mut linedata = data.lines();
+    
+    let settings = linedata.next().expect("something went wrong");
+    
+    let mut settings = settings.split(" ");
+    let board_size: i32 = settings.next().unwrap().parse().unwrap();
+    let start_pos: i32 = settings.next().unwrap().parse().unwrap();
+    let win_value: i32 = settings.next().unwrap().parse().unwrap();
 
-    data.
+    let board = linedata.next().expect("there should be data");
 
-    println!("{data}");
+    println!("{start_pos}");
 }
 
 fn fileToString(path: &str) -> String {
