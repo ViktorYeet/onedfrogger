5c33fb7e7dae2112fcf6f42399f35a5fd8299070
ViktorYeet <viktor@stefansbo.se>
2024-08-22 10:27:32 +0200

    extracted input functions

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,29 @@
 use std::io;
-//use std::fs;
+use std::fs;
+use std::io::stdin;
+use std::str::Lines;
 
 fn main() {
-    //let data = file_to_string("inputdata.txt");
-    //let mut linedata = data.lines();
     
-    //let settings = linedata.next().expect("something went wrong");
-    let mut settings = String::new();
+    
+    let settings = match linedata.next() {
+        Some(line) => line,
+        None => return,
+    };
+
+    /* let mut settings = String::new();
     let stdin = io::stdin();
-    stdin.read_line(&mut settings).expect("there should be data here");
+    stdin.read_line(&mut settings).expect("there should be data here"); */
 
     let mut settings = settings.trim().split(" ");
-    dbg!(&settings);
-    let board_size: i32 = settings.next().unwrap().parse().unwrap();
-    let start_pos: i32 = settings.next().unwrap().parse::<i32>().unwrap() -1;
-    let win_value: i32 = settings.next().unwrap().parse().unwrap();
+    let board_size: isize = settings.next().unwrap().parse().expect("there should be things here");
+    let start_pos: isize = settings.next().unwrap().parse::<isize>().unwrap() -1;
+    let win_value: isize = settings.next().unwrap().parse().expect("there should be things here");
 
-    //let board: Vec<&str> = linedata.next().expect("there should be data").split_whitespace().collect();
-    let mut buffer = String::new();
+    let board: Vec<&str> = linedata.next().expect("there should be data").split_whitespace().collect();
+    /* let mut buffer = String::new();
     stdin.read_line(&mut buffer).expect("there should be data here");
-    let board: Vec<&str> = buffer.split_whitespace().collect();
+    let board: Vec<&str> = buffer.split_whitespace().collect(); */
 
     let mut pos = start_pos.clone();
     let mut hops = 0;
@@ -33,17 +37,36 @@ fn main() {
             println!("cycle\n{hops}");
             return; //going in circles
         }
-        else if pos < 1 || pos > board_size {
-            println!("falls\n{hops}");
+        else if pos.is_negative() {
+            println!("left\n{hops}");
+            return // you lose
+        }
+        else if pos >= board_size {
+            println!("right\n{hops}");
             return // you lose
         }
         visited.push(pos);
-        pos += board[pos as usize].parse::<i32>().unwrap();
+        pos += board[pos as usize].parse::<isize>().unwrap();
         hops += 1;
     }
 }
 
-/* fn file_to_string(path: &str) -> String {
+fn file_to_string(path: &str) -> String {
     let content: String = fs::read_to_string(path).expect("should have read the file");
     content
-} */
+}
+
+fn get_data_from_console() -> String {
+    let mut fline = String::new();
+    let mut sline = String::new();
+    let stdin = io::stdin();
+    stdin.read_line(&mut fline);
+    fline.push_str("\n");
+    stdin.read_line(&mut sline);
+    fline.push_str(&sline);
+    fline
+}
+
+fn get_data_from_file() -> String {
+    file_to_string("inputdata.txt")
+}
